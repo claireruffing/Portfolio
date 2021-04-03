a0799413d8246a1732befa4e824eae95b2c9a59d
Claire Ruffing <cmruffin@ucsc.edu>
2021-04-03 12:22:52 -0700

    Adding artificial intelligence program 3

---
+++ AIProgram3/qlearningAgents.py
@@ -0,0 +1,209 @@
+from pacai.agents.learning.reinforcement import ReinforcementAgent
+from pacai.util import reflection
+
+from pacai.util import counter
+from pacai.util import probability
+import random
+
+class QLearningAgent(ReinforcementAgent):
+    """
+    A Q-Learning agent.
+
+    Some functions that may be useful:
+
+    `pacai.agents.learning.reinforcement.ReinforcementAgent.getAlpha`:
+    Get the learning rate.
+
+    `pacai.agents.learning.reinforcement.ReinforcementAgent.getDiscountRate`:
+    Get the discount rate.
+
+    `pacai.agents.learning.reinforcement.ReinforcementAgent.getEpsilon`:
+    Get the exploration probability.
+
+    `pacai.agents.learning.reinforcement.ReinforcementAgent.getLegalActions`:
+    Get the legal actions for a reinforcement agent.
+
+    `pacai.util.probability.flipCoin`:
+    Flip a coin (get a binary value) with some probability.
+
+    `random.choice`:
+    Pick randomly from a list.
+
+    Additional methods to implement:
+
+    `pacai.agents.base.BaseAgent.getAction`:
+    Compute the action to take in the current state.
+    With probability `pacai.agents.learning.reinforcement.ReinforcementAgent.getEpsilon`,
+    we should take a random action and take the best policy action otherwise.
+    Note that if there are no legal actions, which is the case at the terminal state,
+    you should choose None as the action.
+
+    `pacai.agents.learning.reinforcement.ReinforcementAgent.update`:
+    The parent class calls this to observe a state transition and reward.
+    You should do your Q-Value update here.
+    Note that you should never call this function, it will be called on your behalf.
+
+    DESCRIPTION: <Write something here so we know what you did.>
+    """
+
+    def __init__(self, index, **kwargs):
+        super().__init__(index, **kwargs)
+
+        # You can initialize Q-values here.
+        self.values = counter.Counter()  # A Counter is a dict with default 0
+
+    def getQValue(self, state, action):
+        """
+        Get the Q-Value for a `pacai.core.gamestate.AbstractGameState`
+        and `pacai.core.directions.Directions`.
+        Should return 0.0 if the (state, action) pair has never been seen.
+        """
+        if (state, action) not in self.values:
+            return 0.0
+        return self.values[(state, action)]
+
+    def getValue(self, state):
+        """
+        Return the value of the best action in a state.
+        I.E., the value of the action that solves: `max_action Q(state, action)`.
+        Where the max is over legal actions.
+        Note that if there are no legal actions, which is the case at the terminal state,
+        you should return a value of 0.0.
+
+        This method pairs with `QLearningAgent.getPolicy`,
+        which returns the actual best action.
+        Whereas this method returns the value of the best action.
+        """
+        maxVal = -float("inf")
+        if not self.getLegalActions(state):
+            return 0.0
+        for action in self.getLegalActions(state):
+            value = self.getQValue(state, action)
+            # print("value is: %s" % (str(value)))
+            # Updating the max value with the value iteration sum
+            # and updating temp dict with the maxval to set to self.values in the end
+            if value > maxVal:
+                maxVal = value
+
+        return maxVal
+
+    def getPolicy(self, state):
+        """
+        Return the best action in a state.
+        I.E., the action that solves: `max_action Q(state, action)`.
+        Where the max is over legal actions.
+        Note that if there are no legal actions, which is the case at the terminal state,
+        you should return a value of None.
+
+        This method pairs with `QLearningAgent.getValue`,
+        which returns the value of the best action.
+        Whereas this method returns the best action itself.
+        """
+        maxVal = -float("inf")
+        tempActions = counter.Counter()
+        if not self.getLegalActions(state):
+            return None
+        for action in self.getLegalActions(state):
+            QValue = self.getQValue(state, action)
+            if QValue > maxVal:
+                maxVal = QValue
+                tempActions[action] = maxVal
+        result = tempActions.argMax()
+        # print("result: %s" % (str(result)))
+        return result
+
+    def getAction(self, state):
+        """
+        Compute the action to take in the current state.
+        With probability `pacai.agents.learning.reinforcement.ReinforcementAgent.getEpsilon`,
+        we should take a random action and take the best policy action otherwise.
+        Note that if there are no legal actions, which is the case at the terminal state,
+        you should choose None as the action.
+        """
+        legalActions = self.getLegalActions(state)
+        if not self.getLegalActions(state):
+            return None
+        # getEpsilon() is Get the exploration probability.
+        # flipCoin is Flip a coin(get a binary value) with some probability.
+        # random.choice is Pick randomly from a list.
+        if probability.flipCoin(self.getEpsilon()):
+            return random.choice(legalActions)
+        else:
+            return self.getPolicy(state)
+
+    def update(self, state, action, nextState, reward):
+        """
+        The parent class calls this to observe a state transition and reward.
+        You should do your Q-Value update here.
+        Note that you should never call this function, it will be called on your behalf.
+        Claire Notes: update to V(s) is from temporal differnce
+        """
+        currentQValue = self.getQValue(state, action)
+        self.values[(state, action)] = (1 - self.getAlpha()) * currentQValue + \
+            (self.getAlpha()) * (reward + (self.getDiscountRate() * self.getValue(nextState)))
+
+
+class PacmanQAgent(QLearningAgent):
+    """
+    Exactly the same as `QLearningAgent`, but with different default parameters.
+    """
+
+    def __init__(self, index, epsilon = 0.05, gamma = 0.8, alpha = 0.2, numTraining = 0, **kwargs):
+        kwargs['epsilon'] = epsilon
+        kwargs['gamma'] = gamma
+        kwargs['alpha'] = alpha
+        kwargs['numTraining'] = numTraining
+
+        super().__init__(index, **kwargs)
+
+    def getAction(self, state):
+        """
+        Simply calls the super getAction method and then informs the parent of an action for Pacman.
+        Do not change or remove this method.
+        """
+
+        action = super().getAction(state)
+        self.doAction(state, action)
+
+        return action
+
+class ApproximateQAgent(PacmanQAgent):
+    """
+    An approximate Q-learning agent.
+
+    You should only have to overwrite `QLearningAgent.getQValue`
+    and `pacai.agents.learning.reinforcement.ReinforcementAgent.update`.
+    All other `QLearningAgent` functions should work as is.
+
+    Additional methods to implement:
+
+    `QLearningAgent.getQValue`:
+    Should return `Q(state, action) = w * featureVector`,
+    where `*` is the dotProduct operator.
+
+    `pacai.agents.learning.reinforcement.ReinforcementAgent.update`:
+    Should update your weights based on transition.
+
+    DESCRIPTION: <Write something here so we know what you did.>
+    """
+
+    def __init__(self, index,
+            extractor = 'pacai.core.featureExtractors.IdentityExtractor', **kwargs):
+        super().__init__(index, **kwargs)
+        self.featExtractor = reflection.qualifiedImport(extractor)
+
+        # You might want to initialize weights here.
+
+    def final(self, state):
+        """
+        Called at the end of each game.
+        """
+
+        # Call the super-class final method.
+        super().final(state)
+
+        # Did we finish training?
+        if self.episodesSoFar == self.numTraining:
+            # You might want to print your weights here for debugging.
+            # *** Your Code Here ***
+            raise NotImplementedError()
